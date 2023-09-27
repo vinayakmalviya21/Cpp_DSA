86da1b0c855fbf56f161b9edeb34f6602a3e0118
hp <vinayakmalviya21@gmail.com>
2023-09-27 23:13:43 +0530

    day 2 commit

--- 44Vector.cpp
+++ 44Vector.cpp
@@ -157,8 +157,30 @@ int main()
     // }
 
     // * ----------- 7. Sort 0's and 1's
-        
+    
+    vector<int> arr{0,1,0,1,1,0,1,0,1,1};
 
+    int start = 0;
+    int end = arr.size()-1;
+    int i = 0;
+
+    while(i != end){
+        if(arr[i]==0){
+            // swap from left
+            swap(arr[start],arr[i]);
+            i++;
+            start++;
+        }
+        else{
+            // swap from right
+            swap(arr[i],arr[end]);
+            end--;
+        }
+    }
+
+    for(auto val : arr){
+        cout<<val<<" ";
+    }
 
     return 0;
 }
