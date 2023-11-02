a7d23fe4c480040ae963eb316999e1333b9bb41d
hp <vinayakmalviya21@gmail.com>
2023-11-02 15:15:31 +0530

    18th day commit

---
+++ 86_BasicMath.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std; 
+
+int main(){
+
+    // * Basic Mathematics
+
+    // ! Greatest Common Divisor
+
+    // gcd(a,b) = gcd(a%b,b)  a>b
+    // ~          Or         
+    // gcd(a,b) = gcd(a-b,b)  a>b
+    //            gcd(b-a,a)  a<b
+    // Apply this till one of parameters becomes 0
+
+    //  Ex: gcd(72,24)
+    //      = gcd(48,24)
+    //      = gcd(24,24)
+    //      = gcd(0,24)
+    //      = 24 is GCD
+
+    // ! LCM (a,b) * GCD (a,b)  = a*b
+
+    // & Advanced Topics
+    // 1. Pigeon Hole
+    // 2. Catalan number (BST)
+    // 3. Inclusion - Exclusion Principle
+    // 4.Chinese Remainder Theorem
+    // 5.Lucas' Theorem
+    // 6.Fermat's Theorem
+    // 7. Probability Concepts
+
+
+    return 0;
+}
